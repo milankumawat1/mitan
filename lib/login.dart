d3bdfb73989789e926084fbb68cc4a046d3db472
Milan Kumawat <milankumawat01@gmail.com>
2022-11-25 00:06:23 +0530

    login page

---
+++ lib/login.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class login extends StatelessWidget {
+  const login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Login(),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/logo.jpg'),
+          Text(
+            'Choose Account Type',
+            style: TextStyle(fontSize: 32),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/corporater.png'),
+              SizedBox(
+                width: 40,
+              ),
+              Image.asset('assets/farmer.png')
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 50,
+            width: 280,
+            alignment: Alignment.center,
+            child: Text(
+              'CONTINUE',
+              style: TextStyle(color: Colors.white),
+            ),
+            decoration: new BoxDecoration(
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Already Have An Account?',
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
+                'Login',
+                style: TextStyle(
+                    color: Color.fromRGBO(49, 129, 98, 100),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+}
