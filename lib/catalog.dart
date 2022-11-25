bf9a7815a5b0db516d5a9834076b425849a07593
Milan Kumawat <milankumawat01@gmail.com>
2022-11-25 15:53:16 +0530

    7 pages done    with floating button

--- lib/catalog.dart
+++ lib/catalog.dart
@@ -251,9 +251,226 @@ class catalog extends StatelessWidget {
           ),
         ]),
       ),
+      body: catalogBody(),
+      floatingActionButton: Container(
+        width: 100,
+        child: new FloatingActionButton(
+          onPressed: () {},
+          tooltip: 'Buy',
+          child: Text(
+            '   BUY   ',
+          ),
+          elevation: 4.0,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(40),
+                  bottomLeft: Radius.circular(20))),
+          backgroundColor: Color.fromARGB(255, 28, 175, 104),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedFontSize: 15,
+        selectedIconTheme: IconThemeData(
+          color: Color.fromARGB(255, 0, 0, 0),
+          size: 30,
+        ),
+        selectedItemColor: Color.fromARGB(255, 0, 0, 0),
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        // iconSize: 40,
+        elevation: 2,
+        backgroundColor: Color.fromARGB(135, 255, 255, 255),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.store_outlined),
+            label: 'Shop',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.space_bar),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.wb_cloudy_outlined),
+            label: 'Weather',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.connected_tv_outlined),
+            label: 'Blogs',
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
 
-
 // Image.asset('assets/appbar_leading.png'),
+
+class catalogBody extends StatelessWidget {
+  const catalogBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Image.asset('assets/discountBanner.png'),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 0, 0, 10),
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Categories',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 34),
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    SizedBox(width: 20),
+                    Container(
+                      width: 125,
+                      height: 125,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage("assets/Catalog/Scroll/c1.png"),
+                        fit: BoxFit.cover,
+                      )),
+                    ),
+                    SizedBox(width: 20),
+                    Container(
+                      width: 125,
+                      height: 125,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage("assets/Catalog/Scroll/c2.png"),
+                        fit: BoxFit.cover,
+                      )),
+                    ),
+                    SizedBox(width: 20),
+                    Container(
+                      width: 125,
+                      height: 125,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          image: DecorationImage(
+                            image: AssetImage("assets/Catalog/Scroll/c3.png"),
+                            fit: BoxFit.cover,
+                          )),
+                    ),
+                    SizedBox(width: 20),
+                    // Container(
+                    //   child: Image.asset('assets/Catalog/Scroll/c2.png'),
+                    // ),
+                    // Container(
+                    //   child: Image.asset('assets/Catalog/Scroll/c3.png'),
+                    // ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(children: [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    child: Image.asset('assets/Catalog/Scroll/1.png'),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    child: Image.asset('assets/Catalog/Scroll/2.png'),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    child: Image.asset('assets/Catalog/Scroll/3.png'),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    child: Image.asset('assets/Catalog/Scroll/4.png'),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                ]),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: 400,
+                height: 76.41,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage("assets/Catalog/banner.png"),
+                  fit: BoxFit.cover,
+                )),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 30, 0, 10),
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'News releated to you',
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 30),
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    SizedBox(width: 20),
+                    Container(
+                      width: 250,
+                      height: 140,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage("assets/Catalog/Scroll/n1.png"),
+                        fit: BoxFit.cover,
+                      )),
+                    ),
+                    SizedBox(width: 20),
+                    Container(
+                      width: 250,
+                      height: 140,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage("assets/Catalog/Scroll/n1.png"),
+                        fit: BoxFit.cover,
+                      )),
+                    ),
+                    SizedBox(width: 20),
+
+                    // Container(
+                    //   child: Image.asset('assets/Catalog/Scroll/c2.png'),
+                    // ),
+                    // Container(
+                    //   child: Image.asset('assets/Catalog/Scroll/c3.png'),
+                    // ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
